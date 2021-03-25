b83ba5c406e096893c01623a77c426685db71505
Dmitriy Dolmatov <Rabmail@mail.ru>
2021-03-25 18:07:55 +0300

    Add Test2

--- src/test/java/ru/netology/TestApp.java
+++ src/test/java/ru/netology/TestApp.java
@@ -4,8 +4,11 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.time.Duration;
+import java.util.Locale;
 
+import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selectors.withText;
 import static com.codeborne.selenide.Selenide.*;
 
 public class TestApp {
@@ -18,18 +21,22 @@ public class TestApp {
 
     @Test
     void shouldTestValidation() {
-        $("[data-test-id=city] input").setValue("Белгород");
-        $("[data-test-id=date] input").setValue(SetTest.setDate(3));
-        $("[data-test-id=name] input").setValue("Пупкин Василий");
-        $("[data-test-id=phone] input").setValue("+79102280000");
+        $("[data-test-id=city] input").setValue(TestSet.setCity());
+        $("[data-test-id=date] input").setValue(TestSet.setDate(3));
+        $("[data-test-id=name] input").setValue(TestSet.setFIO());
+        $("[data-test-id=phone] input").setValue(TestSet.setPhone());
         $("[data-test-id=agreement]").click();
         $$("button").get(1).click();
-        //$("[data-test-id='notification']").waitUntil(visible, 15000).shouldHave(text("Успешно!"));
-        //$("[data-test-id=notification]").shouldHave((visible),Duration.ofSeconds(15));
-        //$(withText("Встреча успешно забронирована на")).shouldBe(visible, Duration.ofSeconds(15));
-        $("[data-test-id=notification]").shouldBe((visible), Duration.ofSeconds(15));
-    }
-//    @Test
+        $("[data-test-id='success-notification']").waitUntil(visible, 15000).shouldHave(text("Успешно!"));
+
+        $("[data-test-id=date] input").setValue(TestSet.setDate(5));
+        $(withText("Запланировать")).click();
+        $$("button").get(1).click();
+        $(withText("Перепланировать")).click();
+        $("[data-test-id='success-notification']").waitUntil(visible, 15000).shouldHave(text("Успешно!"));
+        }
+
+            //    @Test
 //    void shouldTestWorkingDropDownMenuCity() {
 //        $("[data-test-id=city] input").sendKeys("Бе", Keys.ARROW_DOWN, Keys.ENTER);
 //        $("[data-test-id=date] input").setValue(SetDate(3));
---
+++ src/test/java/ru/netology/TestSet.java
@@ -0,0 +1,32 @@
+package ru.netology;
+
+import com.github.javafaker.Faker;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
+
+
+public class TestSet {
+    private static Faker faker = new Faker(new Locale("ru"));
+
+    private  TestSet(){};
+
+    public static String setDate(int date) {
+        return LocalDate.now().plusDays(date).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+    }
+
+    public static String setFIO (){
+        return faker.name().fullName();
+    }
+
+    public static String  setCity(){
+       return faker.address().cityName();
+       }
+
+    public static String setPhone(){
+         return faker.phoneNumber().phoneNumber();
+    }
+
+}
